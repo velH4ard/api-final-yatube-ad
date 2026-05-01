f9483dd63c95a39babf0ada32179b910570080c5
Alex <ugabuga.gmail.com>
2026-05-01 14:48:30 +0300

    Fix flake8 issues in generated files

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, permissions, viewsets
 
-from posts.models import Comment, Follow, Group, Post
+from posts.models import Follow, Group, Post
 from .serializers import (CommentSerializer, FollowSerializer,
                           GroupSerializer, PostSerializer)
 
